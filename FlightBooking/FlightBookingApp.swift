7da18035edb30669191b07d492258b0e307f0d3e
idevscode <mail2dilshadali@gmail.com>
2025-07-04 08:11:45 +0530

    Initial Commit

---
+++ FlightBooking/FlightBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightBookingApp.swift
+//  FlightBooking
+//
+//  Created by dilshad haidari on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FlightBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightBookingTests/FlightBookingTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlightBookingTests.swift
+//  FlightBookingTests
+//
+//  Created by dilshad haidari on 04/07/25.
+//
+
+import Testing
+
+struct FlightBookingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
